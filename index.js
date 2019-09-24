5a80b61c96145862365b83a22c851e32289ac996
Nolan Woods <nolan_w@sfu.ca>
2019-09-24 13:49:51 -0700

    Add index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+//TODO import the Vue objects and expand on below
+export default {
+ install(Vue, options) {
+  // https://vuejs.org/v2/guide/components-registration.html
+  //Vue.component("", );
+ }
+};
