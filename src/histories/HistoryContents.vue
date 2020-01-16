568f96cdcb59280455f122f993e42877e265fb83
Nolan Woods <nolan@i2labs.ca>
2020-01-15 17:31:17 -0800

    Clean up dataset search and finish select all hotkey

--- src/histories/HistoryContents.vue
+++ src/histories/HistoryContents.vue
@@ -8,8 +8,8 @@
                     placeholder="Type to Search"
                 />
                 <b-input-group-append>
-                    <b-button :disabled="!search" @click="search_next(true)" title="Previous match"><i class="icon icon-prev"></i></b-button>
-                    <b-button :disabled="!search" @click="search_next(false)" title="Next match"><i class="icon icon-next"></i></b-button>
+                    <b-button :disabled="!search" @click="search_next(true)" title="Previous match"><i class="icon icon-prev"/></b-button>
+                    <b-button :disabled="!search" @click="search_next(false)" title="Next match"><i class="icon icon-next"/></b-button>
                 </b-input-group-append>
             </b-input-group>
 
@@ -37,12 +37,13 @@
                      sort-by="hid"
                      empty-text="Drag and drop files here to upload"
                      thead-class="hidden_header"
+                     :tbody-tr-class="rowClass"
                      @row-selected="row_selected"
                      @dragstart.native.stop.prevent @dragover.native.prevent="upload_dragging=true" @dragleave.native="upload_dragging=false" @dragexit.native="upload_dragging=false" @drop.native.prevent="uploadHandler"
                      ref="table"
                      @keydown.native="handle_hotkey"
             >
-                <template v-slot:cell(item)="row" :class="{'search-target': row.value === search_target}">
+                <template v-slot:cell(item)="row">
                     <DatasetItem
                             v-if="row.value.history_content_type==='dataset'"
                             v-bind:model="row.value"
@@ -87,7 +88,7 @@
      * @param query {String} Search string to compare
      * @param item {HistoryDatasetAssociation || HistoryDatasetCollectionAssociation} item to evaluate
      */
-    search_match(query, item) {
+    function search_match(query, item) {
         return item.hid.toString() === query || item.name.includes(query);
     }
 
@@ -141,7 +142,6 @@
             upload_dragging: false,
             search: '',
             search_target: null,
-            search_target_index: -1,
         }},
         asyncComputed: {
             /**
@@ -173,6 +173,11 @@
             }
         },
         methods: {
+            rowClass(item, type) {
+                if (!item || type !== 'row') return;
+                if (item.item === this.search_target) return 'search-target';
+            },
+
             /**
              * Open the browser file selection dialog
              * @public
@@ -194,31 +199,35 @@
              * @param reverse {Boolean} Search backwards
              */
              search_next(reverse = false) {
-                 const step = reverse ? -1 : 1;
-                 
+                 const step = reverse ? 1 : -1; // 0 is at bottom
+
+                let index = this.items.findIndex(item=>item.item === this.search_target);
+
                  // Ensure current index is not more than one step from items bounds and wrap if needed
-                 this.search_target_index += step;
-                 if (this.search_target_index < 0) this.search_target_index = reverse ? this.items.length-1 : 0;
-                 else if (this.search_target_index >= this.items.length) this.search_target_index = reverse ? this.items.length-1 : 0;
-                 const start_index = this.search_target_index;
+                 index += step;
+                 if (index < 0) index = reverse ? 0 : this.items.length-1;
+                 else if (index >= this.items.length) index = reverse ? 0 : this.items.length-1;
+                 const start_index = index;
                  
                  // Iterate until next match
                  do {
-                     const item = this.items[this.search_target_index].item;
-                     if (search_match(query, item)) {
+                     const item = this.items[index].item;
+                     if (search_match(this.search, item)) {
                          // Match found
                          this.search_target = item;
                          
                          // Scroll to new target
-                         //TODO
+                         const tbody = this.$refs.table.$el.querySelector('tbody');
+                         const row = tbody.querySelectorAll('tr')[this.items.length - index -1];
+                         row.scrollIntoView();
                          return;
                      }
-                     this.search_target_index += step;
+                     index += step;
                      
                      // Wrap in appropriate direction
-                     if (this.search_target_index < 0) this.search_target_index = this.items.length-1;
-                     else if (this.search_target_index >= this.items.length) this.search_target_index = 0;
-                 } while (this.search_target_index !== start_index) // Don't wrap past start_index
+                     if (index < 0) index = this.items.length-1;
+                     else if (index >= this.items.length) index = 0;
+                 } while (index !== start_index); // Don't wrap past start_index
                  
                  // No next match found
                  this.search_target = null;
@@ -274,12 +283,11 @@
              * @param evt {KeyPressEvent} event object
              */
             handle_hotkey(evt) {
-                //TODO
-                if ((evt.ctrl || evt.command) && evt.key === 'a') {
-                    this.$refs.table.select(this.items);
-                    return false;
+                if ((evt.ctrlKey || evt.metaKey) && evt.key === 'a') {
+                    this.$refs.table.selectAllRows();
+                    evt.preventDefault();
+                    evt.stopPropagation();
                 }
-                return true;
             },
 
             /**
@@ -292,8 +300,7 @@
             search() {
                 if (!this.search) {
                     this.search_target = null;
-                    this.search_target_index = -1; // Always start new query searching from top 
-                } else if (this.search_target === null || search_match(this.search, this.search_target)) {
+                } else if (this.search_target === null || !search_match(this.search, this.search_target)) {
                     // Only search next if no current target or current target doesn't match
                     this.search_next();
                 }
@@ -311,8 +318,8 @@
 </script>
 
 <style scoped>
-    >>> .selection-target {
-        border: 1px dotted grey;
+    >>> .search-target {
+        outline: 1px dotted grey;
     }
 
     >>> .hidden_header {
